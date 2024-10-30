6848669b925fe784033d1f043cc297335a61657b
coolshou <jimmy_yeh@alphanetworks.com>
2024-10-30 10:53:24 +0800

    add debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/qonyx-send2boox/issues
+# Bug-Submit: https://github.com/<user>/qonyx-send2boox/issues/new
+# Changelog: https://github.com/<user>/qonyx-send2boox/blob/master/CHANGES
+# Documentation: https://github.com/<user>/qonyx-send2boox/wiki
+# Repository-Browse: https://github.com/<user>/qonyx-send2boox
+# Repository: https://github.com/<user>/qonyx-send2boox.git
